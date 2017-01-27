f93972eb922fd588679d1c266881008c44a24fb2
Coby <ratchet3789@users.noreply.github.com>
2017-01-28 01:05:19 +1000

    Update MouseFunctions.cs
    Comments tweaked

--- Assets/Code/MouseFunctions.cs
+++ Assets/Code/MouseFunctions.cs
@@ -6,9 +6,9 @@ using UnityEngine;
 
 public class MouseFunctions : MonoBehaviour {
 
-    bool bCanDestroy = false; //On overlap of Object with Destroyable Tag, sets to true, on LMB destroys object.
-    public Button ResetButton;
-    public string SceneID;
+    bool bCanDestroy = false; //On overlap of Object with Destroyable Tag, sets to true, on Left Mouse destroys object.
+    public Button ResetButton; //The UI Reset Button
+    public string SceneID; //The Level Name
 
     void Start()
     {
@@ -16,12 +16,12 @@ public class MouseFunctions : MonoBehaviour {
         SceneID = SceneManager.GetActiveScene().name; //Get Scene Name, then set it to SceneID for reloading (Line 44)
     }
 
-	void FixedUpdate()
+	void FixedUpdate() //Runs at a fixed interveal. Update() runs per-frame. FixedUpdate will not change, whereas Update changed based on your current Framerate
     {
-        RaycastHit hit; //Checks for objects overlapping
+        RaycastHit hit; //Creates a list of hit objects
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition); //Get Cursor position on Screen
 
-        Physics.Raycast(ray, out hit); //Cast from camera to Cursor Position
+        Physics.Raycast(ray, out hit); //Cast from the Camera to the cursor position
 
         if(hit.transform != null) //If an object was hit
         {
@@ -33,11 +33,11 @@ public class MouseFunctions : MonoBehaviour {
 
         if(Input.GetMouseButtonDown(0)) //When Left Mouse is Pressed Down
             if (bCanDestroy) //If destroy is true
-                Destroy(hit.transform.gameObject); //When LMB Held, destroy object
+                Destroy(hit.transform.gameObject); //When LMB pressed, destroy object
     }
 
     void RefreshScene()
     {
-        SceneManager.LoadScene(SceneID); //Load the SceneID aka current level
+        SceneManager.LoadScene(SceneID); //Load the SceneID aka current level when Refresh button pressed
     }
 }
